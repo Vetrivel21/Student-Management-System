2b476ba83470b3db03c6b5bc0912769472c76c81
Vetrivel21 <77160819+Vetrivel21@users.noreply.github.com>
2021-12-16 19:46:41 +0530

    Update models.py

--- student/models.py
+++ student/models.py
@@ -1,1 +1,12 @@
+from django.db import models
 
+# Create your models here.
+
+class student(models.Model):
+    name = models.CharField(max_length=200, blank=True)
+    email = models.CharField(max_length=100, blank=True)
+    address = models.TextField(blank=True)
+    gender = models.CharField(max_length=100, blank=True)
+
+    def __str__(self):
+        return self.name
