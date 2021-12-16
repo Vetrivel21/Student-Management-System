b342c5964159c25b0a1deb2c01d65c2e4404e718
Vetrivel21 <77160819+Vetrivel21@users.noreply.github.com>
2021-12-16 19:49:13 +0530

    Update views.py

--- student/views.py
+++ student/views.py
@@ -1,1 +1,54 @@
+from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+from rest_framework import status
+
+from .models import student
+from .serializers import StudentSerializer
+
+@api_view(['GET','POST'])
+def student_list(request):
+
+    if request.method == 'GET':
+        students = student.objects.all()
+        serializer = StudentSerializer(students, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = StudentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def student_details(request, pk):
+    try:
+        students = student.objects.get(pk=pk)
+    except student.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = StudentSerializer(students)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = StudentSerializer(students, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        students.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+
+
+
+
+# Create your views here.
 
