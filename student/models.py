3d52ea73eec746074f1dbbf87b0743fcb9cbc41d
Vetrivel21 <77160819+Vetrivel21@users.noreply.github.com>
2021-12-21 20:15:25 +0530

    Update models.py

--- student/models.py
+++ student/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 
-# Create your models here.
 
 class student(models.Model):
     name = models.CharField(max_length=200, blank=True)
     email = models.CharField(max_length=100, blank=True)
     address = models.TextField(blank=True)
     gender = models.CharField(max_length=100, blank=True)
+    course = models.CharField(max_length=100, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+    updated_at = models.DateTimeField(auto_now_add=True, null=True)
 
     def __str__(self):
         return self.name
