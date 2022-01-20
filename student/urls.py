1a7ca8741f6de1f5233f08f6fdd94773f5d1ae51
Vetrivel21 <77160819+Vetrivel21@users.noreply.github.com>
2022-01-20 12:10:16 +0530

    Update urls.py

--- student/urls.py
+++ student/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from .import views
 urlpatterns = [
     path('api/student/',  views.student_list.as_view(),  name='student-list'),
-    path('api/student/sort/page/',  views.student_LIST.as_view(),  name='student-LIST'),
     path('api/student/filter/', views.student_filter.as_view(), name='student-filter'),
     path('api/student/<int:pk>/', views.student_details, name='student-details'),
 ]
